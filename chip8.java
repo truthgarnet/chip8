e01692c1ad123a9210c36f589bd1de3b06533285
purekonges <purekonges@addone.co.kr>
2026-07-14 15:42:24 +0900

    feat: add Chip8 class scaffolding with core state fields

---
+++ chip8.java
@@ -0,0 +1,15 @@
+public class chip8 {
+    // 필드: 메모리, V 레지스터, PC, I 레지스터, opcode, 스택, 스택포인터, 타이머 2개, 디스플레이, 키보드
+    byte[] memory = new byte[4096];
+    int[] V = new int[16];
+    int I = 0;
+    int opcode;
+    int pc = 0x200;
+    int[] stack = new int[16];
+    int sp = 0;
+    int soundTimer;
+    int delayTimer;
+    boolean[][] display = new boolean[64][32]; // [x][y]
+    boolean[] pressed = new boolean[16];
+}
+
