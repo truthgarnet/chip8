205530fa349f471c56942cccc21a51677be41b89
purekonges <purekonges@addone.co.kr>
2026-07-21 17:05:49 +0900

    feat: add implement 0xA, 0xB, 0xC - 0xA: set index - 0xB: jump with offset - 0xC: random, bitwise AND

--- chip8.java
+++ chip8.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class chip8 {
     // 필드: 메모리, V 레지스터, PC, I 레지스터, opcode, 스택, 스택포인터, 타이머 2개, 디스플레이, 키보드
     byte[] memory = new byte[4096];
@@ -20,7 +22,7 @@ public class chip8 {
     void decode() {
         int nn, nnn, x, y;
         switch ((opcode & 0xF000) >> 12) {
-            case 0:
+            case 0x0:
                 switch ((opcode & 0x00FF)) {
                     case 0xE0: // clear screen
                         for (int displayX = 0; displayX < display.length; displayX++) {
@@ -38,55 +40,55 @@ public class chip8 {
                         break;
                 }
                 break;
-            case 1: // jump
+            case 0x1: // jump
                 nnn = opcode & 0x0FFF;
                 pc = nnn;
                 break;
-            case 2: // call
+            case 0x2: // call
                 stack[sp] = pc;
                 sp++;
                 nnn = opcode & 0x0FFF;
                 pc = nnn;
                 break;
-            case 3: // skip conditionally
+            case 0x3: // skip conditionally
                 nn = opcode & 0x00FF;
                 x = (opcode & 0x0F00) >> 8;
                 if (V[x] == nn) {
                     pc += 2;
                 }
                 break;
-            case 4: // skip conditionally
+            case 0x4: // skip conditionally
                 nn = opcode & 0x00FF;
                 x = (opcode & 0x0F00) >> 8;
                 if (V[x] == nn) {
                     pc += 2;
                 }
                 break;
-            case 5: // skip conditionally 
+            case 0x5: // skip conditionally 
                 x = (opcode & 0x0F00) >> 8;
                 y = (opcode & 0x00F0) >> 4;
                 if (V[x] == V[y]) {
                     pc += 2;
                 }
                 break;
-            case 9: // skip conditionally 
+            case 0x9: // skip conditionally 
                 x = (opcode & 0x0F00) >> 8;
                 y = (opcode & 0x00F0) >> 4;
                 if (V[x] != V[y]) {
                     pc += 2;
                 }
                 break;
-            case 6: // set
+            case 0x6: // set
                 x = (opcode & 0x0F00) >> 8;
                 nn = opcode & 0x00FF;
                 V[x] = nn;
                 break;
-            case 7: // add
+            case 0x7: // add
                 x = (opcode & 0x0F00) >> 8;
                 nn = opcode & 0x00FF;
                 V[x] += nn;
                 break;
-            case 8: 
+            case 0x8: 
                 int n = opcode & 0x000F;
                 switch (n) {
                     case 0x0: // set
@@ -139,6 +141,20 @@ public class chip8 {
                     default:
                         break;
                 }
+            case 0xA: // set index
+                I = opcode & 0x0FFF;
+                break;
+            case 0xB: // jump with offset
+                nnn = opcode & 0x0FFF;
+                pc = nnn + V[0];
+                break;
+            case 0xC: // random
+                Random random = new Random();
+                int r = random.nextInt(256);
+                nn = opcode & 0x00FF;
+                x = (opcode & 0x0F00) >> 12;
+                V[x] = r & nn;
+                break;
         }
     }
 }
